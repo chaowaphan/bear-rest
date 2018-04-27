f2b00fa78203169a9a8490451e7a23c49d179d6a
chaowaphan <5810110080@psu.ac.th>
2018-04-27 14:12:15 +0700

    supprt create bear

--- server/src/index.js
+++ server/src/index.js
@@ -8,6 +8,7 @@ var bears = [
     { id: '1', name: 'Ple' },
     { id: '2', name: 'Tum' }
 ]; 
+var last_bear_id = 3;
   
 router.route('/bears') 
     .get(function(req, res) {
@@ -16,6 +17,7 @@ router.route('/bears')
     .post(function(req, res) { 
         var bear = {}; 
         bear.name = req.body.name; 
+        bear.id = "" + (last_bear_id++);
         bears.push(bear); 
         res.json({ message: 'Bear created!' }); 
     })
@@ -26,6 +28,6 @@ router.route('/bears/:id')
     }) 
 
 app.use(cors());
-
+// all of our routes will be prefixed with /api 
 app.use('/api', bodyParser.json(), router); 
 app.listen(8000);
